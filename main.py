67e6bb72581cc2dbde704c5ef229b1aa11a726eb
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-05-30 15:14:07 +0200

    use new options in main.py example

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from dummy_data import get_features_and_labels
+from dummy_data import get_features_and_labels_regression, get_features_and_labels_classification
 from feature_selection import greedy_feature_selection
 from model import train_RF_and_apply, train_SVC_and_apply
+from sklearn.metrics import mean_absolute_error, accuracy_score
 
-features, labels = get_features_and_labels()
-selection = greedy_feature_selection(features=features, labels=labels, model_function=train_RF_and_apply)
-# features, labels = get_features_and_labels()
-# selection2 = greedy_feature_selection(features=features, labels=labels, model_function=train_SVC_and_apply)
+# # optimize the feature selection for a regression task with a random forest model and metric mean absolute error
+features, labels = get_features_and_labels_regression()
+selection = greedy_feature_selection(features=features, labels=labels, model_function=train_RF_and_apply,
+                                     metric_function=mean_absolute_error)
+
+# optimize the feature selection for a classification task with a SVM as model and classification accuracy metric
+features, labels = get_features_and_labels_classification()
+selection2 = greedy_feature_selection(features=features, labels=labels, model_function=train_SVC_and_apply,
+                                      metric_function=accuracy_score, minimize_metric=False)

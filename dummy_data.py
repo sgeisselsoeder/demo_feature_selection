230682508a05d8f5eacaa1f9d95b1655219272be
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-05-30 15:23:39 +0200

    Thanks to Annika Stiehl for the feedback

--- dummy_data.py
+++ dummy_data.py
@@ -5,7 +5,7 @@ def get_features_and_labels_regression():
     # from sklearn.datasets import make_regression
     # X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
 
-    # 100 timesteps, 5 features, 1 label
+    # 100 time steps, 5 features, 1 label
     dummy_data = np.random.randint(0, 100, size=(100, 4))
     dummy_labels = np.reshape(np.arange(0.0, 10.0, 0.1), (-1, 1))
     labels_np = dummy_labels * 10.0 + 12.5
@@ -15,6 +15,7 @@ def get_features_and_labels_regression():
 
 def get_features_and_labels_classification():
     features_np, labels_np = get_features_and_labels_regression()
+    # classification deals with categorical instead of continuous labels
     labels_np[labels_np <= 58.0] = 0
     labels_np[labels_np > 58.0] = 1
     return features_np, labels_np.astype(int)

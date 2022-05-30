b5641280ead0fdad9d30a6070d14c703627acd94
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-05-30 14:56:22 +0200

    uses SVC model function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dummy_data import get_features_and_labels
 from feature_selection import greedy_feature_selection
-from model import train_RF_and_apply
+from model import train_RF_and_apply, train_SVC_and_apply
 
 features, labels = get_features_and_labels()
 selection = greedy_feature_selection(features=features, labels=labels, model_function=train_RF_and_apply)
+# features, labels = get_features_and_labels()
+# selection2 = greedy_feature_selection(features=features, labels=labels, model_function=train_SVC_and_apply)

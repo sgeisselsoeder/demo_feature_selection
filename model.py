6a46659ad99453d6a45d8001e1e28f8e96449560
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-05-30 14:48:59 +0200

    adds SVC model function

--- model.py
+++ model.py
@@ -1,8 +1,15 @@
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
 
 
 def train_RF_and_apply(train_features: np.array, train_labels: np.array, test_features: np.array) -> np.array:
+    from sklearn.ensemble import RandomForestRegressor
     forest = RandomForestRegressor().fit(X=train_features, y=train_labels.ravel())
     result = forest.predict(X=test_features)
+    return result
+
+
+def train_SVC_and_apply(train_features: np.array, train_labels: np.array, test_features: np.array) -> np.array:
+    from sklearn.svm import SVC
+    model = SVC(kernel='linear').fit(X=train_features, y=train_labels.ravel())
+    result = model.predict(X=test_features)
     return result
